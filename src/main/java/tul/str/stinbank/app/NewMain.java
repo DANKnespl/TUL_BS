74d9bb1d1f47a592d043198d3626d7e292e09ce8
Daniel_Knespl <daniel.knespl@tul.cz>
2023-05-08 14:39:09 +0200

    created Main function to start Spring

--- src/main/java/tul/str/stinbank/app/NewMain.java
+++ src/main/java/tul/str/stinbank/app/NewMain.java
@@ -4,17 +4,32 @@
  */
 package tul.str.stinbank.app;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import org.springframework.boot.SpringApplication;
+import tul.str.stinbank.definedClasses.Account;
+import tul.str.stinbank.definedClasses.Currency;
+import tul.str.stinbank.definedClasses.User;
+import tul.str.stinbank.services.CurrenciesFetchService;
+import tul.str.stinbank.services.FileLoaderService;
+
 /**
  *
  * @author Tommy
  */
 public class NewMain {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private static ArrayList<Currency> Currencies = new ArrayList();
+    private static ArrayList<Account> Accounts = new ArrayList();
+    private static ArrayList<User> Users = new ArrayList();
+    
+    public static void main(String[] args) throws IOException{
+        CurrenciesFetchService.fetchCurrencies("https://www.cnb.cz/cs/financni-trhy/devizovy-trh/kurzy-devizoveho-trhu/kurzy-devizoveho-trhu/denni_kurz.txt","target/classes/data/kurzy.txt");
+        Currencies = FileLoaderService.loadCurrencies("target/classes/data/kurzy.txt");
+        Accounts = FileLoaderService.loadAccounts("target/classes/data/Accounts.txt","target/classes/data/Transactions.txt",Currencies);
+        Users = FileLoaderService.loadUsers("target/classes/data/Users.txt",Accounts);
+        
+        SpringApplication.run(NewMain.class, args);
     }
     
 }
