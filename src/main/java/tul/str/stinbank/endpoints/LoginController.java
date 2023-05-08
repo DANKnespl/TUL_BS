ad91229d8d41c97cc75ef25165183644e51d7002
Daniel_Knespl <daniel.knespl@tul.cz>
2023-05-08 17:03:10 +0200

    fixin imports again

--- src/main/java/tul/str/stinbank/endpoints/LoginController.java
+++ src/main/java/tul/str/stinbank/endpoints/LoginController.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tul.str.stinbank.app.NewMain;
+import tul.str.stinbank.definedClasses.User;
 
 /**
  *
@@ -19,7 +21,7 @@ public class LoginController {
     @CrossOrigin()
     @RequestMapping("/login")
     public int login(String email, String password){
-        ArrayList<User> users = NewMain.Users;
+        ArrayList<User> users = NewMain.getUsers();
         System.out.println(email+" "+password);
         for(int i=0;i<users.size();i++){
             if(email == null ? users.get(i).getEmail() == null : email.equals(users.get(i).getEmail())){
