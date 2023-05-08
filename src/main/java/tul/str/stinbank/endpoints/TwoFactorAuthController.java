68c3bdd446b938e970d086b0f7ef3ace9d87917a
Daniel_Knespl <daniel.knespl@tul.cz>
2023-05-08 15:49:45 +0200

    implementing two factor auth

--- src/main/java/tul/str/stinbank/endpoints/TwoFactorAuthController.java
+++ src/main/java/tul/str/stinbank/endpoints/TwoFactorAuthController.java
@@ -4,10 +4,14 @@
  */
 package tul.str.stinbank.endpoints;
 
+import java.util.ArrayList;
+import java.util.Random;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tul.str.stinbank.app.NewMain;
 import tul.str.stinbank.definedClasses.User;
+import tul.str.stinbank.services.EmailService;
 
 /**
  *
@@ -19,10 +23,27 @@ public class TwoFactorAuthController {
     @CrossOrigin()
     @RequestMapping("/2FA")
     public boolean checkFactor(int userID, String FKey){
-        throw new UnsupportedOperationException();
+        ArrayList<User> users=NewMain.getUsers();
+        if (userID >= users.size()){
+            return false;
+        }
+        return users.get(userID).getFactorKey() == null ? FKey == null : users.get(userID).getFactorKey().equals(FKey);
     }
 
     public User generate2FA(User user){
-        throw new UnsupportedOperationException();
+        try{
+            String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            Random rand = new Random();
+            int length = 6;
+            StringBuilder sb = new StringBuilder(length);
+            for (int i = 0; i < length; i++) {
+                sb.append(characters.charAt(rand.nextInt(characters.length())));
+            }
+            user.setFactorKey(sb.toString());
+            EmailService.sendEmail(user.getEmail(), sb.toString());
+        }catch(Exception e){
+            user.setFactorKey(user.getPass());
+        }
+        return user;
     }
 }
